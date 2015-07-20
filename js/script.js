5cdee48d2aadc7d2584f95fd25a640ce42c45ea0
TheBlueLab <elias@thebluelab.co>
2015-07-20 23:44:46 +0200

    created document.write

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 var answer = Math.random();
+
+if (answer <= 0.250) {
+  document.write("<p>Yes</p>");
+} else if (answer > 0.250 && answer <= 0.500){
+    document.write("<p>Yes, you can!</p>");
+} else if (answer > 0.500 && answer <= 0.750) {
+  document.write("<p>You will succeed</p>");
+} else {
+  document.write("<p>You have my permission</p>");
+}
